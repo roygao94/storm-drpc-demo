acd1008dab906625a5b21a1093dbbcfb96b55a13
roy.gao <roy.gao@cootek.cn>
2016-05-18 17:53:54 +0800

    Remove debugging information.

--- multilang/resources/parse.py
+++ multilang/resources/parse.py
@@ -3,7 +3,7 @@ import storm
 
 class ParserBolt(storm.BasicBolt):
     def process(self, tup):
-        open('/home/roy/output.txt', 'a').write(str(tup) + '\n')
+        # open('/home/roy/output.txt', 'a').write(str(tup) + '\n')
         # id = tup.values[0]
         # result = tup.values[1] + ' -- parsing by Python'
         storm.emit([tup.values[0] + ' -- parsing by Python', tup.values[1]])
